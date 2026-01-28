0c72baf784c7d7881bab8e37df6215cf8addb5e1
elias <eliaslallouet59@gmail.com>
2026-01-28 16:45:57 +0100

    dcvcxsd

--- app.py
+++ app.py
@@ -172,9 +172,6 @@ def create_app() -> Flask:
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
-
